43033581fd47a1a1b88f5a37ddaed3b9023f9942
Salman Zuberi Hussain(Sal Hasan) <96196317+salman1hasan@users.noreply.github.com>
2023-06-28 05:49:10 -0400

    Add files via upload

---
+++ hooks/useUser.tsx
@@ -0,0 +1,42 @@
+import {User} from "@supabase/auth-helpers-nextjs"
+import {useSessionContext,useUser as useSupaUser} from "@supabase/auth-helpers-react";
+import { useEffect, useState, createContext, useContext } from 'react';
+
+import {Subscription, UserDetails} from "@/types";
+
+type UserContextType={
+  accessToken: string | null;
+  user: User | null;
+  userDetails: UserDetails | null;
+  isLoading: boolean;
+  subscription: Subscription
+}
+
+export const UserContext = createContext<UserContextType | undefined>(
+  undefined
+)
+
+export interface Props{
+  [propName: string]: any;
+}
+
+export const MyUserContextProvider = (props: Props) =>{
+  const {
+    session,
+    isLoading: isLoadingUser,
+    supabaseClient: supabase
+  } = useSessionContext();
+  const user = useSupaUser();
+  const accessToken = session?.access_token ?? null;
+  const [isLoadingData, setIsloadingData] = useState(false);
+  const [userDetails, setUserDetails] = useState<UserDetails | null>(null);
+  const [subscription, setSubscription] = useState<Subscription | null>(null);
+}
+
+const getUserDetails = () => supabase.from('users').select('*').single();
+const getSubscription = () =>
+    supabase
+      .from('subscriptions')
+      .select('*, prices(*, products(*))')
+      .in('status', ['trialing', 'active'])
+      .single();
