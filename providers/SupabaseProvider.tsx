979869e6ebdc45b86aa22c332fb1132fc4fb9bec
Salman Zuberi Hussain(Sal Hasan) <96196317+salman1hasan@users.noreply.github.com>
2023-06-30 07:15:35 -0400

    Add files via upload

--- providers/SupabaseProvider.tsx
+++ providers/SupabaseProvider.tsx
@@ -1,6 +1,9 @@
 "use client";
 
-import {useState} from "react"
+import { useState } from "react";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+
 import { Database } from "@/types_db";
 
 interface SupabaseProviderProps {
@@ -10,6 +13,15 @@ interface SupabaseProviderProps {
 const SupabaseProvider: React.FC<SupabaseProviderProps> = ({
   children
 }) => {
-  const [supabaseClient] = useState
-    
+    const [supabaseClient] = useState(() =>
+    createClientComponentClient<Database>()
+  );
+
+  return ( 
+    <SessionContextProvider supabaseClient={supabaseClient}>
+      {children}
+    </SessionContextProvider>
+  );
 }
+ 
+export default SupabaseProvider;
