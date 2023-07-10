e1fb768e41316d89461e687de230d77b78d0a912
Salman Zuberi Hussain(Sal Hasan) <96196317+salman1hasan@users.noreply.github.com>
2023-07-10 15:14:12 -0400

    Add files via upload

---
+++ components/UploadModal.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import useUploadModal from "@/hooks/useUploadModal";
+import Modal from "./Modal"
+
+const UploadModal= () =>{
+  const uploadModal= useUploadModal();
+
+  const onChange =(open:boolean)=>{
+    if(!open){
+      uploadModal.onClose();
+    }
+  }
+
+  return (
+    <Modal
+      title="Upload modal title"
+      description="Upload modal description"
+      isOpen ={uploadModal.isOpen}
+      onChange={()=>{}}
+      >
+        Upload Content
+    </Modal>
+  )
+}
+
+export default UploadModal;
